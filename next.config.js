27256a692f15127101a2b2c2358d75fb7baddea8
emonhossainraihan <mdemon7475@gmail.com>
2022-03-28 19:49:07 +0600

    downgrade packages for vercel deploy

---
+++ next.config.js
@@ -0,0 +1,53 @@
+const withImages = require("next-images");
+const withYAML = require("next-yaml");
+//const { withSentryConfig } = require("@sentry/nextjs");
+// const withTM = require("next-transpile-modules")(["next-mdx-remote"])
+
+// module.exports = withTM({
+//   // ...further Next.js config
+// })
+
+let config = {
+  target: "serverless",
+};
+
+config = {
+  ...config,
+  sassOptions: {
+    additionalData: `
+@use "sass:math";
+@import "styles/palettes.scss";
+@import "styles/theme.scss";
+@import "styles/mixins.scss";
+    `,
+  },
+};
+
+config = {
+  ...config,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
+};
+
+config = withImages(config);
+
+config = withYAML(config);
+
+// remove basePath and assetPrefix for local dev
+module.exports =   {
+// withTM({
+
+  ...config,
+  // basePath: '/testing_b',
+  // assetPrefix: '.'
+  // experimental: {
+	// 	esmExternals: "loose",
+	// },
+}
+// )
